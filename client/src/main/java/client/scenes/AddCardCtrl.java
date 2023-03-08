43888ab70f63402626f6d524f685ed806261f894
Eviatar Hadasi <E.Hadasi@student.tudelft.nl>
2023-03-08 22:23:25 +0100

    Create AddCard controller and fxml scene

---
+++ client/src/main/java/client/scenes/AddCardCtrl.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2021 Delft University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package client.scenes;
+
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class AddCardCtrl {
+
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+
+    @FXML
+    private TextField title;
+
+    @Inject
+    public AddCardCtrl(ServerUtils server, MainCtrl mainCtrl) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+
+    }
+
+    public void cancel() {
+        clearFields();
+        mainCtrl.showOverview();
+    }
+
+//    TODO
+//    public void ok() {
+//        try {
+//            server.addQuote(getQuote());
+//        } catch (WebApplicationException e) {
+//
+//            var alert = new Alert(Alert.AlertType.ERROR);
+//            alert.initModality(Modality.APPLICATION_MODAL);
+//            alert.setContentText(e.getMessage());
+//            alert.showAndWait();
+//            return;
+//        }
+//
+//        clearFields();
+//        mainCtrl.showOverview();
+//    }
+
+    private Card getCard() {
+        return new Card(title.getText());
+    }
+
+    private void clearFields() {
+        title.clear();
+    }
+
+    public void keyPressed(KeyEvent e) {
+        switch (e.getCode()) {
+            case ENTER:
+                System.out.println("ENTER pressed");
+//                ok();
+                break;
+            case ESCAPE:
+                cancel();
+                break;
+            default:
+                break;
+        }
+    }
+}
