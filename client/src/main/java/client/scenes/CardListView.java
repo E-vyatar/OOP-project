8ba25f96d9a67da7a8d9fbb45c3f1227366a13c1
Kajetan <k.m.neumann@student.tudelft.nl>
2023-03-10 15:29:46 +0100

    Added CardListView class to client.

---
+++ client/src/main/java/client/scenes/CardListView.java
@@ -0,0 +1,33 @@
+package client.scenes;
+
+import commons.CardList;
+import javafx.scene.control.TitledPane;
+
+public class CardListView extends TitledPane {
+
+    private CardListViewCtrl controller;
+
+    private CardList cardList;
+
+
+    public CardListView(CardList cardList, CardListViewCtrl controller) {
+        this.cardList = cardList;
+        this.controller = controller;
+
+        createView();
+    }
+
+    public void createView() {
+        this.setCollapsible(false);
+        this.setText(cardList.getCardListTitle());
+    }
+
+    public CardList getCardList() {
+        return cardList;
+    }
+
+    public void setCardList(CardList cardList) {
+        this.cardList = cardList;
+        createView();
+    }
+}
