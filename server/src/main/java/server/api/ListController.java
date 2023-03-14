afcdb2cc8ca9ad2c71d5bcfb31e384f017a7145f
Kajetan <k.m.neumann@student.tudelft.nl>
2023-03-14 22:53:42 +0100

    Added ListController constructor

--- server/src/main/java/server/api/ListController.java
+++ server/src/main/java/server/api/ListController.java
@@ -2,8 +2,15 @@ package server.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import server.database.ListRepository;
 
 @RestController
 @RequestMapping("/lists")
 public class ListController {
+
+    private final ListRepository listRepository;
+
+    public ListController(ListRepository listRepository) {
+        this.listRepository = listRepository;
+    }
 }
