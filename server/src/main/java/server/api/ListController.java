412791517c0e80350bf3f764d4d166e1f014739e
Kajetan <k.m.neumann@student.tudelft.nl>
2023-03-14 23:05:01 +0100

    Added GET(/lists/all)

--- server/src/main/java/server/api/ListController.java
+++ server/src/main/java/server/api/ListController.java
@@ -1,9 +1,14 @@
 package server.api;
 
+import commons.CardList;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import server.database.ListRepository;
 
+/**
+ *
+ */
 @RestController
 @RequestMapping("/lists")
 public class ListController {
@@ -12,5 +17,15 @@ public class ListController {
 
     public ListController(ListRepository listRepository) {
         this.listRepository = listRepository;
+    }
+
+    /**
+     * Get all lists
+     *
+     * @return all lists
+     */
+    @GetMapping("all")
+    public Iterable<CardList> getAllLists() {
+        return listRepository.findAll();
     }
 }
