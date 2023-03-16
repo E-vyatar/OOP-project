6497fa24b75f0f67d72d35a23879b7e250bc3efa
Abigail de Nooij <j.denooij@student.tudelft.nl>
2023-03-16 11:13:21 +0100

    refactor: move the stage for the card popup to the CardPopupCtrl
    This refactor moves the stage for the card popup to CardPopupCtrl. This allows MainCtrl to be significantly shorted and makes the code more modular. I also removed the ServerUtils and MainCtrl from CardPopupCtrl since they were unused.

--- client/src/main/java/client/scenes/CardPopupCtrl.java
+++ client/src/main/java/client/scenes/CardPopupCtrl.java
@@ -1,31 +1,42 @@
 package client.scenes;
 
-import client.utils.ServerUtils;
 import commons.Card;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import org.apache.commons.lang3.NotImplementedException;
-
-import javax.inject.Inject;
 
 public class CardPopupCtrl {
 
-    private final ServerUtils utils;
-    private final MainCtrl mainCtrl;
+    private Stage cardPopup;
 
     public Card card;
 
+    @FXML
+    private Parent root;
     @FXML
     private TextField cardTitle;
     @FXML
     private TextArea cardDescription;
 
-    @Inject
-    public CardPopupCtrl(ServerUtils utils, MainCtrl mainCtrl) {
-        this.utils = utils;
-        this.mainCtrl = mainCtrl;
+    /**
+     * This initializes the controller.
+     * Note that we only create the stage now,
+     * because otherwise the root would not be set yet
+     * and creating the scene would throw a NullPointerException
+     */
+    public void initialize() {
+        this.cardPopup = new Stage();
+        this.cardPopup.initModality(Modality.WINDOW_MODAL);
+        this.cardPopup.setMinWidth(240.0);
+        this.cardPopup.setMinHeight(200.0);
+        this.cardPopup.setScene(new Scene(root));
     }
+
 
     public void setCard(Card card) {
         this.card = card;
@@ -39,14 +50,26 @@ public class CardPopupCtrl {
 
     }
 
+    /**
+     * This function closes the popup.
+     * It is called by pressing the close button in the popup.
+     */
     @FXML
     private void close() {
-        mainCtrl.hideCard();
+        this.cardPopup.hide();
     }
 
     @FXML
     private void save() {
         // TODO: allow to save data when editing card
         throw new NotImplementedException("Saving changes hasn't been implemented yet.");
+    }
+
+    /**
+     * This function shows the popup.
+     * Before calling it, you should call the {@link #setCard(Card)} method.
+     */
+    public void show(){
+        this.cardPopup.show();
     }
 }
