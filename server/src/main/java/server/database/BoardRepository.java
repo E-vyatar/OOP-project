cb414c353e089b253fbe769935defb540d1f65b6
romir kulshrestha <r.kulshrestha@student.tudelft.nl>
2023-03-30 15:12:53 +0200

    unused import

--- server/src/main/java/server/database/BoardRepository.java
+++ server/src/main/java/server/database/BoardRepository.java
@@ -2,7 +2,6 @@ package server.database;
 
 import commons.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 @SuppressWarnings("unused")
 public interface BoardRepository extends JpaRepository<Board, Long> {
