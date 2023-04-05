606bc272b81681cda5711057c9c2ef2cea746d72
Kajetan <k.m.neumann@student.tudelft.nl>
2023-04-05 14:51:15 +0200

    Add the 5 tests for moveCardMessage

--- server/src/test/java/websockets/CardControllerWSEndpointTest.java
+++ server/src/test/java/websockets/CardControllerWSEndpointTest.java
@@ -1,6 +1,7 @@
 package websockets;
 
 import commons.Card;
+import commons.messages.MoveCardMessage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,6 +31,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
+/**
+ * Test the Web Socket endpoints for /cards path (in CardController)
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     classes = {CardController.class, WebSocketConfig.class, Main.class})
 public class CardControllerWSEndpointTest {
@@ -48,6 +52,11 @@ public class CardControllerWSEndpointTest {
         this.webSocketStompClient = new WebSocketStompClient(new StandardWebSocketClient());
     }
 
+    /**
+     * Tests the /cards/new socket endpoint
+     *
+     * @throws Exception for the .get() in websocket connection
+     */
     @Test
     void addMessage() throws Exception {
         Card card = new Card(1L, 0L, 1L, "Card 1", 0);
@@ -86,6 +95,11 @@ public class CardControllerWSEndpointTest {
         verifyNoMoreInteractions(cardRepository);
     }
 
+    /**
+     * Tests the /cards/edit socket endpoint
+     *
+     * @throws Exception for the .get() in websocket connection
+     */
     @Test
     void updateCardMessage() throws Exception {
         Card card1 = new Card(1L, 1L, 1L, "Card 1", 0);
@@ -131,6 +145,11 @@ public class CardControllerWSEndpointTest {
         verifyNoMoreInteractions(cardRepository);
     }
 
+    /**
+     * Tests the /cards/delete socket endpoint
+     *
+     * @throws Exception for the .get() in websocket connection
+     */
     @Test
     void deleteMessage() throws Exception {
 
@@ -174,8 +193,282 @@ public class CardControllerWSEndpointTest {
         verifyNoMoreInteractions(cardRepository);
     }
 
+    /**
+     * Tests the /cards/move socket endpoint.
+     * <p>
+     *     Test Case: card does not exist
+     * </p>
+     *
+     * @throws Exception for the .get() in websocket connection
+     */
     @Test
-    void moveCardMessage() throws Exception {
-        // TODO
+    void moveCardMessage1() throws Exception {
+        MoveCardMessage message = new MoveCardMessage(0, 1, 0, 1);
+
+        when(cardRepository.findById(message.getCardId())).thenReturn(Optional.empty());
+
+        BlockingQueue<MoveCardMessage> blockingQueue = new ArrayBlockingQueue<>(1);
+
+        webSocketStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSession session = webSocketStompClient
+            .connect(
+                String.format("ws://localhost:%d/websocket", port),
+                new StompSessionHandlerAdapter() {})
+            .get(1, SECONDS);
+
+        session.subscribe("/topic/cards/move", new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return MoveCardMessage.class;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                blockingQueue.add((MoveCardMessage) payload);
+            }
+        });
+
+        session.send("/app/cards/move", message);
+
+        await()
+            .atMost(1, SECONDS)
+            .untilAsserted(() -> {
+                Assertions.assertNotNull(blockingQueue.peek());
+                Assertions.assertFalse(blockingQueue.poll().isMoved());
+            });
+
+        verify(cardRepository, times(1)).findById(message.getCardId());
+        verifyNoMoreInteractions(cardRepository);
+    }
+
+    /**
+     * Tests the /cards/move socket endpoint.
+     * <p>
+     *     Test Case: card is moved to different list
+     * </p>
+     *
+     * @throws Exception for the .get() in websocket connection
+     */
+    @Test
+    void moveCardMessage2() throws Exception {
+        MoveCardMessage message = new MoveCardMessage(0, 1, 0, 1);
+        Card card = new Card(0, 0, 0, "Card", 0);
+
+        when(cardRepository.findById(message.getCardId())).thenReturn(Optional.of(card));
+        doNothing()
+            .when(cardRepository)
+            .moveAllCardsHigherEqualThanIndexUp(message.getNewListId(), message.getNewIndex());
+        doNothing()
+            .when(cardRepository)
+            .moveAllCardsHigherThanIndexDown(card.getListId(), card.getIdx());
+        when(cardRepository.save(card)).thenReturn(null);
+
+        BlockingQueue<MoveCardMessage> blockingQueue = new ArrayBlockingQueue<>(1);
+
+        webSocketStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSession session = webSocketStompClient
+            .connect(
+                String.format("ws://localhost:%d/websocket", port),
+                new StompSessionHandlerAdapter() {})
+            .get(1, SECONDS);
+
+        session.subscribe("/topic/cards/move", new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return MoveCardMessage.class;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                blockingQueue.add((MoveCardMessage) payload);
+            }
+        });
+
+        session.send("/app/cards/move", message);
+
+        await()
+            .atMost(1, SECONDS)
+            .untilAsserted(() -> {
+                Assertions.assertEquals(1, card.getIdx());
+                Assertions.assertEquals(1, card.getListId());
+                Assertions.assertNotNull(blockingQueue.peek());
+                Assertions.assertTrue(blockingQueue.poll().isMoved());
+            });
+
+        verify(cardRepository, times(1)).findById(message.getCardId());
+        verify(cardRepository, times(1))
+            .moveAllCardsHigherEqualThanIndexUp(1, 1);
+        verify(cardRepository, times(1))
+            .moveAllCardsHigherThanIndexDown(0, 0);
+        verify(cardRepository, times(1)).save(card);
+        verifyNoMoreInteractions(cardRepository);
+    }
+
+    /**
+     * Tests the /cards/move socket endpoint.
+     * <p>
+     *     Test Case: card moved to same index & same list
+     * </p>
+     *
+     * @throws Exception for the .get() in websocket connection
+     */
+    @Test
+    void moveCardMessage3() throws Exception {
+        MoveCardMessage message = new MoveCardMessage(0, 0, 0, 0);
+        Card card = new Card(0, 0, 0, "Card", 0);
+
+        when(cardRepository.findById(message.getCardId())).thenReturn(Optional.of(card));
+
+        BlockingQueue<MoveCardMessage> blockingQueue = new ArrayBlockingQueue<>(1);
+
+        webSocketStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSession session = webSocketStompClient
+            .connect(
+                String.format("ws://localhost:%d/websocket", port),
+                new StompSessionHandlerAdapter() {})
+            .get(1, SECONDS);
+
+        session.subscribe("/topic/cards/move", new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return MoveCardMessage.class;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                blockingQueue.add((MoveCardMessage) payload);
+            }
+        });
+
+        session.send("/app/cards/move", message);
+
+        await()
+            .atMost(1, SECONDS)
+            .untilAsserted(() -> {
+                Assertions.assertEquals(0, card.getIdx());
+                Assertions.assertEquals(0, card.getIdx());
+                Assertions.assertNotNull(blockingQueue.peek());
+                Assertions.assertTrue(blockingQueue.poll().isMoved());
+            });
+
+        verify(cardRepository, times(1)).findById(message.getCardId());
+        verifyNoMoreInteractions(cardRepository);
+    }
+
+    /**
+     * Tests the /cards/move socket endpoint.
+     * <p>
+     *     Test Case: card moved to same list but index lower
+     * </p>
+     *
+     * @throws Exception for the .get() in websocket connection
+     */
+    @Test
+    void moveCardMessage4() throws Exception {
+        MoveCardMessage message = new MoveCardMessage(0, 0, 0, 0);
+        Card card = new Card(0, 0, 0, "Card", 1);
+
+        when(cardRepository.findById(message.getCardId())).thenReturn(Optional.of(card));
+        doNothing().when(cardRepository).updateIdxBetweenUp(0, 0, 1);
+        when(cardRepository.save(card)).thenReturn(null);
+
+        BlockingQueue<MoveCardMessage> blockingQueue = new ArrayBlockingQueue<>(1);
+
+        webSocketStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSession session = webSocketStompClient
+            .connect(
+                String.format("ws://localhost:%d/websocket", port),
+                new StompSessionHandlerAdapter() {})
+            .get(1, SECONDS);
+
+        session.subscribe("/topic/cards/move", new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return MoveCardMessage.class;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                blockingQueue.add((MoveCardMessage) payload);
+            }
+        });
+
+        session.send("/app/cards/move", message);
+
+        await()
+            .atMost(1, SECONDS)
+            .untilAsserted(() -> {
+                Assertions.assertEquals(0, card.getIdx());
+                Assertions.assertEquals(0, card.getListId());
+                Assertions.assertNotNull(blockingQueue.peek());
+                Assertions.assertTrue(blockingQueue.poll().isMoved());
+            });
+
+        verify(cardRepository, times(1)).findById(message.getCardId());
+        verify(cardRepository, times(1))
+            .updateIdxBetweenUp(0, 0, 1);
+        verify(cardRepository, times(1)).save(card);
+        verifyNoMoreInteractions(cardRepository);
+    }
+
+    /**
+     * Tests the /cards/move socket endpoint.
+     * <p>
+     *     Test Case: card moved to same list but index higher
+     * </p>
+     *
+     * @throws Exception for the .get() in websocket connection
+     */
+    @Test
+    void moveCardMessage5() throws Exception {
+        MoveCardMessage message = new MoveCardMessage(0, 0, 0, 1);
+        Card card = new Card(0, 0, 0, "Card", 0);
+
+        when(cardRepository.findById(message.getCardId())).thenReturn(Optional.of(card));
+        doNothing().when(cardRepository).updateIdxBetweenDown(0, 0, 1);
+        when(cardRepository.save(card)).thenReturn(null);
+
+        BlockingQueue<MoveCardMessage> blockingQueue = new ArrayBlockingQueue<>(1);
+
+        webSocketStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSession session = webSocketStompClient
+            .connect(
+                String.format("ws://localhost:%d/websocket", port),
+                new StompSessionHandlerAdapter() {})
+            .get(1, SECONDS);
+
+        session.subscribe("/topic/cards/move", new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return MoveCardMessage.class;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                blockingQueue.add((MoveCardMessage) payload);
+            }
+        });
+
+        session.send("/app/cards/move", message);
+
+        await()
+            .atMost(1, SECONDS)
+            .untilAsserted(() -> {
+                Assertions.assertEquals(1, card.getIdx());
+                Assertions.assertEquals(0, card.getListId());
+                Assertions.assertNotNull(blockingQueue.peek());
+                Assertions.assertTrue(blockingQueue.poll().isMoved());
+            });
+
+        verify(cardRepository, times(1)).findById(message.getCardId());
+        verify(cardRepository, times(1))
+            .updateIdxBetweenDown(0, 0, 1);
+        verify(cardRepository, times(1)).save(card);
+        verifyNoMoreInteractions(cardRepository);
     }
 }
